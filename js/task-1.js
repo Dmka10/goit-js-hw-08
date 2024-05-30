00a183a8ead86a4a019e1a02d28b5a781ce8c2ce
Dmka10 <ewq33@i.ua>
2024-05-30 22:07:32 +0300

    hw8

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const mainLiElem = document.querySelectorAll('ul#categories > .item');
-console.log(`Number of categories: ${mainLiElem.length}`);
-
-const allTitle = mainLiElem.forEach(elem => {
-  const titleName = elem.querySelector('h2').textContent;
-  const allItems = elem.querySelectorAll('li').length;
-  console.log(`Category: ${titleName}`);
-  console.log(`Elements: ${allItems}`);
-});
